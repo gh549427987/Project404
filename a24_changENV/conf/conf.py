c55f7ae15606aac45ecc024283112c66b74eb266
gh549427987 <48665305+gh549427987@users.noreply.github.com>
2020-07-13 21:16:35 +0800

    Finish 1.0

---
+++ a24_changENV/conf/conf.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+# @Time    : 2020/7/13 19:25
+# @Author  : 蟹蟹 ！！
+# @FileName: settings.py
+# @Software: PyCharm
+
+import os
+BASE_PATH = os.path.dirname(os.path.dirname(__file__)) # /a24_changENV
+
+REG_PATH = os.path.join(BASE_PATH, 'reg')
